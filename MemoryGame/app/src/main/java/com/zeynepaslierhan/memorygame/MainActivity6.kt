6feb4d222a5af9d9e397261387e14eeee2ea2085
Hazar Koc <hazark41@gmail.com>
2022-12-13 03:57:05 +0300

    oyun zorluğu seçimi

---
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/MainActivity6.kt
@@ -0,0 +1,11 @@
+package com.zeynepaslierhan.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main6)
+    }
+}

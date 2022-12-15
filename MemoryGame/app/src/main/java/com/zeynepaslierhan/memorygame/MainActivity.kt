54f91a005fa3e7bc90abed66ae318b05f2fedd04
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-15 16:37:42 +0300

    Authentication işlemleri yapıldı

--- MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/MainActivity.kt
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.zeynepaslierhan.memorygame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val user = auth.currentUser
+        if(user!=null){
+
+            val intent =Intent(this,oyuncu_secActivity::class.java)
+
+            val usermail = user.email.toString()
+            Toast.makeText(this,"Welcome: ${usermail}",Toast.LENGTH_LONG).show()
+
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun login(view: View){
+
+        val email = EmailText.text.toString()
+        val password = PasswordText.text.toString()
+
+
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+
+                val user = auth.currentUser?.email.toString()
+
+                Toast.makeText(this,"Welcome: ${user}",Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this,oyuncu_secActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener{exception->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun register(view: View){
+
+        val email = EmailText.text.toString()
+        val password = PasswordText.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+
+                val user = auth.currentUser?.email.toString()
+
+                Toast.makeText(this,"Register is successfull: ${user}",Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this,oyuncu_secActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener{exception->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+
     }
 }

6981c9cf84e0d18934be9dfe68e5381b0424055c
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-16 00:58:21 +0300

    dizayn mizayn

--- MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/oyuncu_secActivity.kt
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/oyuncu_secActivity.kt
@@ -3,12 +3,20 @@ package com.zeynepaslierhan.memorygame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 class oyuncu_secActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oyuncu_sec)
+
+        auth= FirebaseAuth.getInstance()
     }
 
     fun single(view: View){
@@ -18,6 +26,24 @@ class oyuncu_secActivity : AppCompatActivity() {
     fun multiplayer(view: View){
         //val intent = Intent(this,zorluk_secActivity::class.java)
         //startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.LogOut){
+
+            auth.signOut()
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
--- MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/zorluk_secActivity.kt
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/zorluk_secActivity.kt
@@ -3,12 +3,19 @@ package com.zeynepaslierhan.memorygame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 class zorluk_secActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zorluk_sec)
+
+        auth= FirebaseAuth.getInstance()
     }
 
     fun basicGame(view: View){
@@ -22,5 +29,23 @@ class zorluk_secActivity : AppCompatActivity() {
     fun hardGame(view: View){
         val intent = Intent(this,OyunZorActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.LogOut){
+
+            auth.signOut()
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

91131775a74ab91eb17c0e1e9bf77837e3100cca
Hazar Koc <hazark41@gmail.com>
2022-12-20 00:13:38 +0300

    Multi Klasör Yapısı

---
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/Multi/MultiZorActivity.kt
@@ -0,0 +1,11 @@
+package com.zeynepaslierhan.memorygame.Multi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MultiZorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_zor)
+    }
+}

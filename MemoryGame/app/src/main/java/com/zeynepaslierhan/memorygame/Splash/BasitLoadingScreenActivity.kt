649f87c37fb82bc032764a56c7e5769c4b9190d3
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-20 16:47:10 +0300

    StartButton

--- MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/Splash/BasitLoadingScreenActivity.kt
+++ MemoryGame/app/src/main/java/com/zeynepaslierhan/memorygame/Splash/BasitLoadingScreenActivity.kt
@@ -2,26 +2,41 @@ package com.zeynepaslierhan.memorygame.Splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.zeynepaslierhan.memorygame.R
 
 class BasitLoadingScreenActivity : AppCompatActivity() {
+
+    var progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+    var textView = findViewById<TextView>(R.id.textView)
+    var button = findViewById<Button>(R.id.StartButton)
+    lateinit var activity : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basit_loading_screen)
 
-        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
-        val textView = findViewById<TextView>(R.id.textView)
+        activity = intent.getStringExtra("activity").toString()
 
-        val activity = intent.getStringExtra("activity").toString()
+    }
+    fun start(){
 
         progressBar.max = 100
         progressBar.scaleY = 3F
+
+        button.isVisible = false
+        progressBar.isVisible = true
+
         val anim = ProgressBarAnimation(
             this,progressBar,textView,0F,100F,activity
         )
+
         anim.duration=3000
         progressBar.animation = anim
     }
+
 }
+
